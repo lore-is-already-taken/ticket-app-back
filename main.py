91bf0506dca7b77b7996379f80bc2e95d6c0e8d3
lore <lorenzosaavedra.c@gmail.com>
2024-06-03 21:16:49 -0400

    hola

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
-from fastapi import FastAPI
+import time
+
+import uvicorn
+from fastapi import Depends, FastAPI, Request
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
+# app.add_middleware(HTTPSRedirectMiddleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+@app.get("/testdata")
+async def read_main():
+    return {"msg": "Hello World"}
+
+
 @app.get("/")
-def home():
-    return {"message": "HelloWorld!"}
+async def root():
+    return {"msg": "hola mi rey"}
 
 
-@app.get("/helloworld")
-def saludo():
-    return "Saludos waxo"
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)

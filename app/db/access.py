3af0d28bd45b13b23bd635fbef231f1615f0572b
Klithik <santi17461@gmail.com>
2024-06-09 13:13:48 -0400

    dockerized app

--- app/db/access.py
+++ app/db/access.py
@@ -2,14 +2,20 @@ import pyodbc
 
 
 def database_connect():
-    user = "ticket-user"
-    password = "computacionapp123.,"
+    user = "volaos"
+    password = "corxea-fans420!"
 
     server = "database-server-ticketapp.database.windows.net"
-    database = "ticket-app-database"
+    database = "ticket-api-db"
 
-    conection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:{},1433;Database={};Uid={};Pwd={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
+    conection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:{},1433;Database={};UID={};PWD={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
         server, database, user, password
     )
-    conn = pyodbc.connect(conection_string)
+    # connection = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:+"server"+,1433;Database=+"database"+;Uid=+"user"+;Pwd=+"password"+;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    try:
+        conn = pyodbc.connect(conection_string)
+        print("CONNECTED")
+    except Exception as e:
+        print(e)
     return conn
+database_connect()

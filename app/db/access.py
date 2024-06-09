729955e1e3fc45d2fa7030be1c59a249a1f38b25
Klithik <santi17461@gmail.com>
2024-06-09 13:23:04 -0400

    functioning sql driver

--- app/db/access.py
+++ app/db/access.py
@@ -7,15 +7,15 @@ def database_connect():
 
     server = "database-server-ticketapp.database.windows.net"
     database = "ticket-api-db"
+    odbc = "ODBC Driver 18 for SQL Server"
 
-    conection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:{},1433;Database={};UID={};PWD={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
-        server, database, user, password
+    conection_string = "Driver={};Server=tcp:{},1433;Database={};UID={};PWD={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
+        odbc, server, database, user, password
     )
-    # connection = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:+"server"+,1433;Database=+"database"+;Uid=+"user"+;Pwd=+"password"+;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
     try:
         conn = pyodbc.connect(conection_string)
-        print("CONNECTED")
+        print("CONNECTED!!!!!!")
+        return conn
     except Exception as e:
         print(e)
-    return conn
 database_connect()

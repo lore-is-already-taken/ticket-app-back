c75b92481c204378950341a8654fd8add26654fe
Klithik <santi17461@gmail.com>
2024-06-09 21:26:15 -0400

    db working

--- app/db/access.py
+++ app/db/access.py
@@ -1,21 +1,21 @@
-import pyodbc
-
+import pyodbc, struct
+from azure import identity
 
 def database_connect():
     user = "volaos"
     password = "corxea-fans420!"
 
-    server = "database-server-ticketapp.database.windows.net"
+    # server = "database-server-ticketapp.database.windows.net"
+    server = "ticket-api.database.windows.net"
+    # database = "ticket-api-db"
     database = "ticket-api-db"
     odbc = "ODBC Driver 18 for SQL Server"
 
-    conection_string = "Driver={};Server=tcp:{}.database.windows.net,1433;Database={};UID={};PWD={};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;".format(
-        odbc, server, database, user, password
-    )
+    conection_string = f"Driver={odbc};Server=tcp:{server},1433;Database={database};Uid={user};Pwd={password};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;"
     try:
         conn = pyodbc.connect(conection_string)
         print("CONNECTED!!!!!!")
         return conn
     except Exception as e:
         print(e)
-database_connect()
+database_connect() 

4dc636c291b9f636ee92f69cf8ad425fc3a0d603
Klithik <santi17461@gmail.com>
2024-06-10 10:30:03 -0400

    added search user functions

--- app/db/access.py
+++ app/db/access.py
@@ -1,3 +1,4 @@
+from typing import List
 import pyodbc, struct
 from azure import identity
 
@@ -26,22 +27,22 @@ def add_user(name:str,email:str,password:str,rol:str)->bool:
     line = f"INSERT INTO Users (name,email,password) VALUES ('{name}','{email}','{password}');"
     cursor.execute(line)
     cursor.commit()
-    
-    line = f"SELECT userID FROM Users WHERE email='{email}';"
-    cursor.execute(line)
-    rows = []
-    for row in cursor.fetchcall():
-        rows.append(f"{row.userID}")
 
-    line = f"INSER INTO Rol (userID,rol) VALUES ('{rows[0]}','{rol}')"
-    cursor.execute(line)
-    cursor.commit()
+    userID = get_userID_by_email(email)
+    add_rol(userID,rol)
 
     return True
 
 def add_ticket(autor:int,contenido:str,categoria:str,prioridad:int):
     cursor = database_connect().cursor()
     line = f"INSERT INTO Ticket (autor,contenido,categoria,prioridad) VALUES ('{autor}','{contenido}','{categoria}',{prioridad});"
+    cursor.execute(line)
+    cursor.commit()
+    return True
+
+def add_rol(userID:int,rol:str):
+    cursor = database_connect().cursor()
+    line = f"INSERT INTO Rol (userID,rol) VALUES ('{userID}','{rol}');"
     cursor.execute(line)
     cursor.commit()
     return True
@@ -57,9 +58,42 @@ def add_Evento(ticketID:int,contenido:str):
 #   FUNCIONES SELECT
 #####################################################
 
-def get_userID_by_email(email:str):
+def get_userID_by_email(email:str)->int:
+    '''
+    Obtiene el userID de un determinado usuario buscandolo
+    por email, si no lo encuentra o algo sale mal devuelve 0
+    '''
     cursor = database_connect().cursor()
     line = f"SELECT userID FROM Users WHERE email='{email}';"
     cursor.execute(line)
     for row in cursor.fetchcall():
         return row.userID
+    return 0
+
+def get_user_by_ID(id:int)->List[str]:
+    '''
+    Devuelve un arreglo con 2 elementos
+    [nombre,email].
+    Si no encuentra el usuario devuelve un
+    arreglo vacio
+    '''
+    cursor = database_connect().cursor()
+    line = f"SELECT name,email FROM Users WHERE userID='{id}'"
+    cursor.execute(line)
+    res = []
+    for row in cursor.fetchcall():
+        res.append(row.name)
+        res.append(row.email)
+    return res
+
+def get_password_by_email(email:str)->str:
+    '''
+    Obtiene la contraseña de un usuario segun su email.
+    Si no lo encuentra o hubo un error devuelve un string vacio.
+    '''
+    cursor = database_connect().cursor()
+    line = f"SELECT password FROM Users WHERE email='{email}'"
+    cursor.execute(line)
+    for row in cursor.fetchcall():
+        return row.password
+    return ""

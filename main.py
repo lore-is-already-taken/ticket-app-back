2529dc290ffc190d897539142c89806027d10a7b
Klithik <santi17461@gmail.com>
2024-06-14 15:41:26 -0400

    added doc

--- main.py
+++ main.py
@@ -83,6 +83,12 @@ async def drop_user(user: models.onlyToken):
 
 @app.post("/asign_ticket")
 async def asign_ticket(info: models.ticket_user):
+    '''
+    Recibe un token de sesion como entrada para asignar un ticket a un determinado usuario.
+    El objeto consiste de dos campos:
+        - access_token: str
+        - ticket_id: int
+    '''
     try:
         userID = jwt.decode(info.access_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])['user_id']
         if db.asign_responsable(userID,info.ticket_id):

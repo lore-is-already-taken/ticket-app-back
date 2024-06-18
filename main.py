87310e1e60756ef1800ef880750dc80a3d752ebd
Klithik <santi17461@gmail.com>
2024-06-17 21:48:56 -0400

    no se escribir

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ async def assign_ticket(info: models.ticket_user):
         userID = jwt.decode(info.access_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])[
             "user_id"
         ]
-        if db.asign_responsable(userID, info.ticket_id):
+        if db.assign_responsable(userID, info.ticket_id):
             return {"msg": "Success"}
         else:
             raise HTTPException(status_code=501, detail="No se pudo asignar")
